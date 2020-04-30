e321a544a50f7882d48db9ab0c23a56aa77768fa
bensaadalyes <bensaadalyes82@gmail.com>
2020-04-30 14:30:25 +0200

    upgrade extract of txtFileParser

--- src/main/java/org/trsfrm/file/parser/TxtFileParser.java
+++ src/main/java/org/trsfrm/file/parser/TxtFileParser.java
@@ -1,11 +1,8 @@
 package org.trsfrm.file.parser;
 
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -20,11 +17,9 @@ import org.trsfrm.file.attribute.IFileAttributeService;
 import org.trsfrm.file.validator.IFileValidator;
 import org.trsfrm.model.FileSettingsToSend;
 
-
-
 @Service
 @Qualifier("txtFile")
-public  class TxtFileParser extends FileParser {
+public class TxtFileParser extends FileParser {
 
 	@Autowired
 	TxtFileParser(IFileValidator txtFileValidatorService, IFileAttributeService fileAttributeService) {
@@ -39,25 +34,29 @@ public  class TxtFileParser extends FileParser {
 		List listAttribute = fileAttributeService.getAttributes(fileSetting.getDiretoryPath());
 		try {
 			return new Iterator<String>() {
-				List<String> lines = Files.lines(Paths.get(fileSetting.getFile().getPath())).collect(Collectors.toList());
-				
+				List<String> lines = Files.lines(Paths.get(fileSetting.getFile().getPath()))
+						.collect(Collectors.toList());
+
 				Iterator<String> iterator = lines.iterator();
 				String currentLine = null;
-				Map<Integer,String> attributeIndex = extractAttributeIndexes(iterator.hasNext()?iterator.next():null, listAttribute, lineAttributeDelimitor);
-				
+				Map<Integer, String> attributeIndex = extractAttributeIndexes(
+						iterator.hasNext() ? iterator.next() : null, listAttribute, lineAttributeDelimitor);
+
 				@Override
 				public boolean hasNext() {
 					// TODO Auto-generated method stub
-					
+
 					return iterator.hasNext();
 				}
 
 				@Override
 				public String next() {
-					if(attributeIndex == null ) return null;
-					if(iterator.hasNext()) System.out.println("there is data "+attributeIndex.size());
+					if (attributeIndex == null)
+						return null;
+					if (iterator.hasNext())
+						System.out.println("there is data " + attributeIndex.size());
 					currentLine = iterator.next();
-					return extractAttributesValFromLine(currentLine,attributeIndex,lineAttributeDelimitor);
+					return extractAttributesValFromLine(currentLine, attributeIndex, lineAttributeDelimitor);
 				}
 			};
 		} catch (IOException e) {
@@ -81,7 +80,8 @@ public  class TxtFileParser extends FileParser {
 	 */
 	Map extractAttributeIndexes(String firstLineFile, List<String> listAttribute, String delimitor) {
 
-		if(firstLineFile == null) return null;
+		if (firstLineFile == null)
+			return null;
 		Map<Integer, String> listIndex = new HashMap();
 		String[] lineAttributesValue = firstLineFile.split(delimitor);
 		int i = 0;
@@ -99,19 +99,25 @@ public  class TxtFileParser extends FileParser {
 	 * @param line
 	 * @param indexToExtract
 	 * @param delimitor
-	 * @return extract all value of requested attribute on that line, with a form key1=value1, key2=value2
+	 * @return extract all value of requested attribute on that line, with a
+	 *         form of key1=value1, key2=value2
 	 * 
 	 */
 	String extractAttributesValFromLine(String line, Map<Integer, String> indexToExtract, String delimitor) {
 		if (line == null || line.length() == 0)
 			return null;
 		StringBuilder result = new StringBuilder();
+		try{
 		String[] values = line.split(delimitor);
 		for (Entry<Integer, String> entry : indexToExtract.entrySet()) {
 			if (result.length() == 0)
 				result = result.append(entry.getValue() + "=" + values[entry.getKey()]);
 			else
-				result = result.append(","+entry.getValue() + "=" + values[entry.getKey()]);
+				result = result.append("," + entry.getValue() + "=" + values[entry.getKey()]);
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
 		}
 		return result.toString();
 	}
